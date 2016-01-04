f6d7732b43a58239066bcfc5c07081fab02c31a3
Matt Morrisey <mattydevicce@gmail.com>
2016-01-04 15:20:19 -0500

    Made an almost version of the pass triangle... I only made it one directional and needs to be bi directional...

---
+++ maxtriangle/maxtriangle.py
@@ -0,0 +1,44 @@
+def max_triangle(triangle):
+	height = len(triangle)
+	greatest = 0
+	stop = 1
+	current_position = len(triangle[-1])-1
+	positions = [0]*len(triangle[-1])
+	# while positions[-1] != len(triangle[-1])-1:
+	total = 0
+	i = 0
+	while positions[-1] != len(triangle[-1]):
+		i += 1
+
+		for pos in range(0,len(positions)):
+			total += triangle[pos][positions[pos]]
+		
+		if total > greatest:
+			greatest = total
+
+		positions[current_position]+=1
+		if current_position == stop:
+			stop += 1
+			current_position = len(triangle[-1])-1
+		
+		else:
+			current_position-=1
+		total = 0
+
+	while positions[-1] != 0:
+		
+
+
+import sys
+triangle_list = []
+test_cases = open("triangle.txt", 'r')
+i = 0
+for test in test_cases:
+	i += 1
+	test = test.split(" ")
+	if i < 100:
+		test.pop()
+	test = map(int, test)
+	triangle_list.append(test)
+test_cases.close()
+max_triangle(triangle_list)
