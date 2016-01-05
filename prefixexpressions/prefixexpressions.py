2ff4e00badcc90bde48a652e93119942e91d2c29
Matt Morrisey <mattydevicce@gmail.com>
2016-01-05 13:47:49 -0500

    MAde a bunch of edge case tests... got 87.5 on it, I dont know where else it could fail besides maybe if it

--- prefixexpressions/prefixexpressions.py
+++ prefixexpressions/prefixexpressions.py
@@ -1,25 +1,38 @@
 def eval_expressions(number_list, operator_list):
-	total = int(number_list[0])
+	total = eval(number_list[0])
 	for i in range(0,len(operator_list)):
 		if operator_list[i] == '+':
-			total += int(number_list[i+1])
+			total += eval(number_list[i+1])
 		elif operator_list[i] == '*':
-			total *= int(number_list[i+1])
+			total *= eval(number_list[i+1])
 		else:
-			total /= int(number_list[i+1])
+			total /= eval(number_list[i+1])
 
 	print total
 
 import sys
-test_cases = open("pref.txt", 'r')
+test_cases = open(sys.argv[1], 'r')
 for test in test_cases:
-	test = test.split(" ")
-	print len(test)
-	print len(test)/2
-	operators = test[0:int(len(test)/2)]
-	numbers   = test[int(len(test)/2):]
-	operators = operators[::-1]
+	if len(test) == 0:
+		continue
+	elif len(test.split(" "))==1:
+		test = test.split(" ")
+		if float(test[0]):
+			print eval(test[0])
+	elif len(test.split(" "))==2:
+		test = test.split(" ")
+		if float(test[0]):
+			print eval(test[0])
+		elif float(test[1]):
+			print eval(test[1])
+		else:
+			continue
+	else:
+		test = test.split(" ")
+		test[-1] = test[-1].strip()
+		operators = test[0:int(len(test)/2)]
+		numbers   = test[int(len(test)/2):]
+		operators = operators[::-1]
 
-	eval_expressions(numbers, operators)
-
+		eval_expressions(numbers, operators)
 test_cases.close()
